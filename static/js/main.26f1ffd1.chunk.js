1ee4686e55c1e20355b22d37149d06ea2e54708d
chenyuhao <cccchenyuhao@gmail.com>
2020-11-20 23:18:57 +0800

    Updates

---
+++ static/js/main.26f1ffd1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpignite=this.webpackJsonpignite||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),i=(n(0),n(3)),s=function(){return Object(c.jsx)("div",{className:"app",children:"app"})};Object(i.render)(Object(c.jsx)(s,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.26f1ffd1.chunk.js.map
